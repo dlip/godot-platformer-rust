2aac72aaa62905f31a213a5e5769a587938c0784
Dane <dane@lipscombe.com.au>
2021-04-06 09:06:07 +1000

    Update movement

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -15,7 +15,7 @@ impl Player {
     fn new(_owner: &KinematicBody2D) -> Self {
         Player {
             velocity: Vector2::new(0.0, 0.0),
-            gravity: 400.0,
+            gravity: 1000.0,
         }
     }
 
@@ -28,18 +28,19 @@ impl Player {
 
     #[export]
     pub fn _physics_process(&mut self, owner: &KinematicBody2D, delta: f64) {
+        godot_print!("{:?}", self.velocity);
         let input = Input::godot_singleton();
-        self.velocity.y = self.gravity;
+        self.velocity.y += self.gravity * delta as f32;
 
-        let mut movement = Vector2::new(0.0, 0.0);
-        movement.x += Input::get_action_strength(&input, "move_right") as f32;
-        movement.x -= Input::get_action_strength(&input, "move_left") as f32;
+        let mut direction = Vector2::new(0.0, 0.0);
+        direction.x = Input::get_action_strength(&input, "move_right") as f32
+            - Input::get_action_strength(&input, "move_left") as f32;
 
-        if Input::is_action_pressed(&input, "jump") {
-            movement.y -= self.gravity;
+        if owner.is_on_floor() && Input::is_action_pressed(&input, "jump") {
+            direction.y = -15.0;
         }
 
-        self.velocity += movement * 10.0;
+        self.velocity += direction * 50.0;
 
         self.velocity = owner.move_and_slide(
             self.velocity,
