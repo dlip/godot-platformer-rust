2d3b9bd65036e0b04e3b980272355fd4b5b51699
Dane <dane@lipscombe.com.au>
2021-04-06 08:31:44 +1000

    Add some movement

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,12 +4,19 @@ use gdnative::api::OS;
 
 #[derive(NativeClass)]
 #[inherit(KinematicBody2D)]
-struct Player;
+struct Player {
+    velocity: Vector2,
+    #[property(path = "base/gravity")]
+    gravity: f32,
+}
 
 #[gdnative::methods]
 impl Player {
     fn new(_owner: &KinematicBody2D) -> Self {
-        Player
+        Player {
+            velocity: Vector2::new(0.0, 0.0),
+            gravity: 400.0,
+        }
     }
 
     #[export]
@@ -20,24 +27,28 @@ impl Player {
     }
 
     #[export]
-    pub fn _physics_process(&self, owner: &KinematicBody2D, _delta: f64) {
+    pub fn _physics_process(&mut self, owner: &KinematicBody2D, delta: f64) {
         let input = Input::godot_singleton();
-        let mut velocity = Vector2::new(0.0, 0.0);
+        self.velocity.y = self.gravity;
 
-        if Input::is_action_pressed(&input, "ui_right") {
-            velocity.x = 1.0
-        }
-        if Input::is_action_pressed(&input, "ui_left") {
-            velocity.x = -1.0
-        }
-        if Input::is_action_pressed(&input, "ui_down") {
-            velocity.y = 1.0
-        }
-        if Input::is_action_pressed(&input, "ui_up") {
-            velocity.y = -1.0
+        let mut movement = Vector2::new(0.0, 0.0);
+        movement.x += Input::get_action_strength(&input, "move_right") as f32;
+        movement.x -= Input::get_action_strength(&input, "move_left") as f32;
+
+        if Input::is_action_pressed(&input, "jump") {
+            movement.y -= self.gravity;
         }
 
-        owner.move_and_collide(velocity, false, false, false);
+        self.velocity += movement * 10.0;
+
+        self.velocity = owner.move_and_slide(
+            self.velocity,
+            Vector2::new(0.0, 1.0),
+            false,
+            100,
+            45.0,
+            true,
+        );
     }
 }
 
