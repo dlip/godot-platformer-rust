c7dcc80cbca77e755fb54d3b54b104fa70e7145c
Dane <dane@lipscombe.com.au>
2021-04-05 22:31:46 +1000

    Move player around screen

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,7 @@
 use gdnative::prelude::*;
 
+use gdnative::api::OS;
+
 #[derive(NativeClass)]
 #[inherit(KinematicBody2D)]
 struct Player;
@@ -12,12 +14,28 @@ impl Player {
 
     #[export]
     fn _ready(&self, _owner: &KinematicBody2D) {
-        godot_print!("hello, Dane.")
+        let os = OS::godot_singleton();
+        os.center_window();
+        godot_print!("hello, Dane.");
     }
 
     #[export]
     pub fn _physics_process(&self, owner: &KinematicBody2D, _delta: f64) {
-        let velocity = Vector2::new(1.0, 0.0);
+        let input = Input::godot_singleton();
+        let mut velocity = Vector2::new(0.0, 0.0);
+
+        if Input::is_action_pressed(&input, "ui_right") {
+            velocity.x = 1.0
+        }
+        if Input::is_action_pressed(&input, "ui_left") {
+            velocity.x = -1.0
+        }
+        if Input::is_action_pressed(&input, "ui_down") {
+            velocity.y = 1.0
+        }
+        if Input::is_action_pressed(&input, "ui_up") {
+            velocity.y = -1.0
+        }
 
         owner.move_and_collide(velocity, false, false, false);
     }
